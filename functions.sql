3d81feeb6acb96319b19d0de314f8272d597e038
Amir N <nazemi.amirnaser@gmail.com>
2025-10-17 12:25:04 -0500

    Changed artist_id to auto increment, need to update artist_id in the schema to auto increment too

--- functions.sql
+++ functions.sql
@@ -1,4 +1,4 @@
-USING museumdb;
+USE museumdb;
 
 DELIMITER $$
 CREATE PROCEDURE CreateArtist(
@@ -10,13 +10,16 @@ CREATE PROCEDURE CreateArtist(
   IN p_bio text,
   OUT p_artist_id
 )
-  BEGIN
-    START TRANSACTION;
-    SELECT COALESCE(MAX(artist_id),0)+1 INTO p_artist_id
-    FROM ARTIST;
+-- Changed artist_id to auto increment instead of manually assigning IDs
+BEGIN
+  INSERT INTO ARTIST (first_name, last_name, birth_year, death_year, nationality, bio)
+  VALUES (p_first_name, p_last_name, p_birth_year, p_death_year, p_nationality, p_bio);
+  SET p_artist_id = LAST_INSERT_ID();
+END$$
 
-    INSERT INTO ARTIST(artist_id, first_name, last_name, birth_year, death_year, nationality, bio)
-    VALUES(p_artist_id, p_first_name, p_last_name, p_birth_year, p_death_year, p_nationality, p_bio);
-    COMMIT;
-  END$$
 DELIMITER ;
+
+-- TODO: Run in MySQL terminal to make artist_id auto increment
+-- ALTER TABLE ARTIST MODIFY artist_id INT NOT NULL AUTO_INCREMENT,
+-- ALTER TABLE ARTIST AUTO_INCREMENT = 1001; 
+-- Set starting point for artist_id to 1001 but we can change it to something else if needed
