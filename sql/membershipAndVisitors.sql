2a043e9e2a378b453d4de3a37dfc5aeb955140f4
Payam Vakili <159935053+pvakili65@users.noreply.github.com>
2025-10-25 23:49:25 +0000

    Implemented GetDemographics query

--- sql/membershipAndVisitors.sql
+++ sql/membershipAndVisitors.sql
@@ -23,4 +23,23 @@ BEGIN
     ORDER BY MEMBER.member_id;
 END$$
 
+
+--returns how many tickets sold to members who are students/not students and visitors who are students/not students
+CREATE PROCEDURE GetDemographics()
+BEGIN
+    SELECT 'Visitor' AS customer_type, VISITOR.is_student, COUNT(TICKET.ticket_id) AS total_tickets, SUM(TICKET.quantity) AS total_attendees
+    FROM TICKET
+    INNER JOIN VISITOR ON TICKET.visitor_id = VISITOR.visitor_id
+    GROUP BY VISITOR.is_student
+    
+    UNION ALL
+    
+    SELECT 'Member' AS customer_type, MEMBER.is_student, COUNT(TICKET.ticket_id) AS total_tickets, SUM(TICKET.quantity) AS total_attendees
+    FROM TICKET
+    INNER JOIN MEMBER ON TICKET.member_id = MEMBER.member_id
+    GROUP BY MEMBER.is_student
+    
+    ORDER BY customer_type, is_student;
+END$$
+
 DELIMITER ;
