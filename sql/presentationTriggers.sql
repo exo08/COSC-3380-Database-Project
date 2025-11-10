1365b1a104c00091d21f9d2120d9f2d80350dcff
exo08 <sansdaskelley@gmail.com>
2025-11-09 21:58:27 -0600

    Update trg_validate_active_member_sale trigger logic
    Enhanced trigger to check membership status before sale.

--- sql/presentationTriggers.sql
+++ sql/presentationTriggers.sql
@@ -10,14 +10,14 @@ CREATE TRIGGER `trg_reduce_stock_after_sale` AFTER INSERT ON `SALE_ITEM` --Sale 
   WHERE quantity_in_stock < 0;
 END
 
-CREATE TRIGGER `trg_validate_active_member_sale` BEFORE INSERT ON `SALE`
+CREATE TRIGGER `trg_validate_active_member_sale` BEFORE INSERT ON `SALE` -- before completing sale
  FOR EACH ROW BEGIN
-  IF NEW.member_id IS NOT NULL THEN
-    IF NOT EXISTS (
+  IF NEW.member_id IS NOT NULL THEN -- if sale is associated with a member (not visitor), check membership status
+    IF NOT EXISTS ( -- if membership is active
       SELECT 1 FROM MEMBER 
       WHERE member_id = NEW.member_id 
       AND expiration_date >= CURDATE()
-    ) THEN
+    ) THEN -- say cannot apply discount
       SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Member account is expired. Cannot apply member benefits.';
     END IF;
