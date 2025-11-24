02548c37ce4fefb2fadac725ac9519e54af3e5e3
Amir N <nazemi.amirnaser@gmail.com>
2025-11-24 17:06:50 -0600

    Updated config.php template that matches deployed file

--- public/app/config.example.php
+++ public/app/config.example.php
@@ -1,11 +1,11 @@
 <?php
 // Copy this file to config.php and fill in real credentials.
-// date_default_timezone_set('America/Chicago');
+date_default_timezone_set('America/Chicago');
 return [
-    'db_host' => 'sql313.infinityfree.com',
-    'db_user' => 'if0_40178317',
+    'db_host' => 'localhost', // since website and database are on same server (hostinger)
+    'db_user' => 'u452501794_DBuser',
     'db_pass' => '',
-    'db_name' => 'if0_40178317_Museum_DB',
+    'db_name' => 'u452501794_MuseumDB',
     'app_env' => 'development', // production for live site, development for testing
-    'force_https' => true
+    'force_https' => true,
 ];
