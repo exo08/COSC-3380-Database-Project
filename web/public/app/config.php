501e2fd0d1acdb4d077d2efd789400d3702a1795
Amir N. <nazemi.amirnaser@gmail.com>
2025-11-24 01:18:23 -0600

    Changed file structure to match what is on the server for deployment

---
+++ web/public/app/config.php
@@ -0,0 +1,10 @@
+<?php
+// Private config file for the live database on InfinityFree
+return [
+    'db_host' => 'sql313.infinityfree.com',
+    'db_user' => 'if0_40178317',           // FIXED: was 'db_name'
+    'db_pass' => 'Museum2025',
+    'db_name' => 'if0_40178317_Museum_DB',
+    'app_env' => 'development', // production for live site, development for testing
+    'force_https' => true,
+];
